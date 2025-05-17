3cccdc7e187c53ef92298860a6d4a3b7bfcdd04f
AmirZanzuri <amirzanzuri82@gmail.com>
2025-05-17 22:22:40 +0300

    Fix Tailwind CSS configuration and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -12,6 +11,23 @@ module.exports = {
         'dark-blue': 'var(--dark-blue)',
         'darker-blue': 'var(--darker-blue)',
       },
+      opacity: {
+        '10': '0.1',
+        '20': '0.2',
+        '30': '0.3',
+        '40': '0.4',
+        '50': '0.5',
+        '60': '0.6',
+        '70': '0.7',
+        '80': '0.8',
+        '90': '0.9',
+      },
+      borderColor: {
+        'neon-blue': {
+          '20': 'rgba(0, 243, 255, 0.2)',
+          '40': 'rgba(0, 243, 255, 0.4)'
+        }
+      },
       boxShadow: {
         'neon': '0 0 15px var(--neon-blue)',
         'neon-lg': '0 0 30px var(--neon-blue)',
@@ -19,4 +35,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
