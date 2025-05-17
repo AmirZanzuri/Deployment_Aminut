af23559abfc3b8a0d2be7193de690246888a47cf
AmirZanzuri <amirzanzuri82@gmail.com>
2025-05-17 22:08:58 +0300

    Add custom color to Tailwind border colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,24 +11,9 @@ export default {
         'dark-blue': 'var(--dark-blue)',
         'darker-blue': 'var(--darker-blue)',
       },
-      opacity: {
-        '10': '0.1',
-        '20': '0.2',
-        '30': '0.3',
-        '40': '0.4',
-        '50': '0.5',
-        '60': '0.6',
-        '70': '0.7',
-        '80': '0.8',
-        '90': '0.9',
-      },
       borderColor: {
-        'neon-blue': {
-          DEFAULT: 'var(--neon-blue)',
-          '20': 'rgba(0, 243, 255, 0.2)',
-          '40': 'rgba(0, 243, 255, 0.4)'
-        }
-      }
+        'neon-blue': 'var(--neon-blue)',
+      },
     },
   },
   plugins: [],
