727f4e870599dd97a2024f49fbc6cb6c4e87e680
AmirZanzuri <amirzanzuri82@gmail.com>
2025-05-17 22:21:00 +0300

    Update neon theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,32 @@
-// tailwind.config.js
-const plugin = require('tailwindcss/plugin');
-
-function withOpacityValue(variable) {
-  return ({ opacityValue }) =>
-    opacityValue === undefined
-      ? `rgb(var(${variable}))`
-      : `rgb(var(${variable}) / ${opacityValue})`;
-}
-
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        'neon-blue': withOpacityValue('--neon-blue-rgb'),
+        'neon-blue': 'var(--neon-blue)',
         'dark-blue': 'var(--dark-blue)',
         'darker-blue': 'var(--darker-blue)',
       },
+      opacity: {
+        '10': '0.1',
+        '20': '0.2',
+        '30': '0.3',
+        '40': '0.4',
+        '50': '0.5',
+        '60': '0.6',
+        '70': '0.7',
+        '80': '0.8',
+        '90': '0.9',
+      },
       boxShadow: {
-        'neon': '0 0 15px rgb(var(--neon-blue-rgb) / 0.3)',
-        'neon-lg': '0 0 30px rgb(var(--neon-blue-rgb) / 0.3)',
+        'neon': '0 0 15px var(--neon-blue)',
+        'neon-lg': '0 0 30px var(--neon-blue)',
       },
     },
   },
   plugins: [],
-};
+}
