3649105d39c77e4bc7f5580c73df823d46d18387
AmirZanzuri <amirzanzuri82@gmail.com>
2025-05-17 22:13:45 +0300

    Fix Tailwind CSS opacity configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ export default {
         'dark-blue': 'var(--dark-blue)',
         'darker-blue': 'var(--darker-blue)',
       },
+      boxShadow: {
+        'neon': '0 0 15px rgb(var(--neon-blue-rgb) / 0.3)',
+      },
     },
   },
   plugins: [],
